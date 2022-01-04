c6c88d5fde81bae302858b0b6280dcd95d565ed8
enricorov <enricorov@gmail.com>
2022-01-04 16:43:10 +0000

    Updated palette app (still WIP)

--- apps/palette/app.js
+++ apps/palette/app.js
@@ -56,6 +56,11 @@ const BTN_STYLE_DEF = {
   col_bg : WHITE,
   col_text: BLACK,
 }
+const TXT_STYLE_DEF =   {
+  col_bg: WHITE,
+  col_text:BLACK,
+  col_border:GREY_BRIGHT
+};
 
 const BTN_DEFAULT_SIZE = {
   w: 30,
@@ -159,14 +164,22 @@ class Layer {
     this.label = label;
   }
 
-  parseTaps(xy) {
-    this.items.forEach(item => {
-      // // print(item)
-      if (item.was_tapped(xy)) {
-        // pass parent layer to the tapped button
-        item.callback(this);
-      }
-    });
+  parse_event(event, xy) {
+    if(event==="tap"){
+      console.log(`Parsing:${JSON.stringify(xy)}, ${JSON.stringify(event)}`);
+      // search for features in layout with same coords
+      this.layout.forEach(item => {
+        // console.log(item)
+        if (item.was_tapped(xy)) {
+          // pass parent layer to the tapped button
+          item.callback(item, this);
+        }
+      });
+    }
+  }
+
+  callback() {
+    return;
   }
 
   render() {
@@ -194,18 +207,23 @@ class Button {
     }
     
     was_tapped(xy) {
-      var x = xy.x;
-      var y = xy.y;
       
+      // console.log(`Size ${JSON.stringify(this.size)} Origin ${JSON.stringify(this.origin)}`)
+      var x = (xy.x >= g.getWidth())? g.getWidth(): xy.x;
+      var y = (xy.y >= g.getHeight())? g.getHeight(): xy.y;
+      y
       let x1 = this.origin.x;
       let y1 = this.origin.y;
-      let x2 = this.origin.x + this.size.h;
-      let y2 = this.origin.y + this.size.w;
-
+      let x2 = this.origin.x + this.size.w;
+      let y2 = this.origin.y + this.size.h;
+      
+      // console.log(`Did ${JSON.stringify(xy)} tap in (${x1}, ${y1}) (${x2}, ${y2})?`)
     if ((x > x1 && x < x2) && (y > y1 && y < y2)) {
+      console.log(`Tapped ${this.label}`);
       return true;
     }
     else {
+      // console.log("")
       return false;
     }
   }
@@ -232,19 +250,25 @@ class Button {
     return this;
   }
   
+  mark_dirty() {
+    this.render();
+  }
+  
   render() {
-    
+
     let x1 = this.origin.x;
     let y1 = this.origin.y;
     let x2 = this.origin.x + this.size.w;
     let y2 = this.origin.y + this.size.h;
     let border_thickness = 6;
+    const max_size = 20;
 
+    let local_font_size = (this.size.h / 2 > max_size) ? max_size : this.size.h/2;
     g.setColor(this.style.col_border).fillRect(x1, y1, x2, y2);
     g.setColor(this.style.col_bg).fillRect(x1+border_thickness/2, y1+border_thickness/2, x2-border_thickness/2, y2-border_thickness/2);
     g.setColor(this.style.col_text);
 
-    g.setFontAlign(0, 0).setFont("Vector", 20).drawString(this.label, (x1+x2)/2, (y1+y2)/2);
+    g.setFontAlign(0, 0).setFont("Vector", local_font_size).drawString(this.label, (x1+x2)/2, (y1+y2)/2);
   }
 
   set_origin(origin) {
@@ -262,26 +286,56 @@ class Button {
     }
   }
   
-  // --------- Headers finished. Onto Timer stuff.
+  // --------- Headers finished. Onto Timer stuff.W
   
   let tickTimer;
   
   function clearTickTimer() {
+    // console.log('clearTickTimer');
     if (tickTimer) {
       clearTimeout(tickTimer);
       tickTimer = undefined;
     }
   }
   
-  function queueNextTick() {
-    clearTickTimer();
-    tickTimer = setTimeout(tick, 100);
+  function queueNextTick(tick_func) {
+    // console.log('queuedTick');
+    clearTickTimer()
+    tickTimer = setTimeout(function(){
+      tick();
+      let type = typeof tick_func !== "function"? "func" : "Object";
+      console.log(`type: ${type}`);
+      if(typeof tick_func !== "undefined"){
+        tick_func();
+      } else {
+        console.log("tick_func is undefined.");
+      }
+    }, 1000);
   }
   
   function tick() {
     // console.log("tick");
     draw();
-    queueNextTick();
+    queueNextTick(draw);
+  }
+
+  Bangle.on('touch', function (button, xy) {
+  
+    console.log(`Tapped ${JSON.stringify(xy)}`)
+    // window.render();
+    main_window.top_layer().parse_event("tap", xy);
+    main_window.render();
+  
+  });
+  
+  function draw() {
+    main_window.render();
+  }
+  
+  if(!Bangle.isLocked())
+  {
+    console.log("Unlocking");
+    Bangle.setLocked(false);
   }
 
   console.log(`So far so good..`)
@@ -290,37 +344,60 @@ class Button {
   
   // Clear the screen once, at startup
   g.clear();
-
-  var main_window = new Window("palette", BLACK);
+  
+  var main_window = new Window("main", BLACK);
   var layer_buttons = new Layer();
+  var layer_palette = new Layer("palette");
+  
   
 var grid_layout = [];
 
 const top_left_x = 3;
-const top_left_y = 3;
+const top_left_y = 20;
 const width = 20;
 const height = 20;
 const spacing = 3
 
-const N =10;
-const M =10;
-// Generate sample layout
-for (let i = 0; i < N; i++) {
-  for (let j = 0; j < M; j++) {
+// const N =2;
+// const M =2;
+// // Generate sample layout
+// for (let i = 0; i < N; i++) {
+//   for (let j = 0; j < M; j++) {
     
-    grid_layout.push(
-      {
-        type: "btn", 
-        origin: {
-        x: top_left_x + (width + spacing)*i ,
-        y: top_left_y + (height + spacing)*j,
-        },
-        size: {w:width, h:height}, 
-        // label: `${i},${j}`,
-        style: {col_bg: (i%2===1)?GREEN:RED, col_text: BLACK, col_border:WHITE}
-      });
-    }
-}
+//     grid_layout.push(
+//       {
+//         type: "btn", 
+//         origin: {
+//         x: top_left_x + (width + spacing)*i ,
+//         y: top_left_y + (height + spacing)*j,
+//         },
+//         size: {w:width, h:height}, 
+//         label: `${i},${j}`,
+//         style: {col_bg: (i%2===1)?GREEN:RED, col_text: BLACK, col_border:WHITE}
+//       });
+//     }
+// }
+
+var ok_layout = [
+  {
+    type: "btn",
+    origin: {
+      x: 3, y: 3
+    },
+    size: {w:_screen_mid, h:40},
+    style: {col_bg:RED},
+    label: "Tap away!"
+  },
+  {
+    type: "btn",
+    origin: {
+      x: 3, y: 100
+    },
+    size: { w: _screen_mid * 2, h: _screen_mid - 3 },
+    style: {col_bg:RED},
+    label: ""
+  },
+]
 
 var ok_layout = [
   {
@@ -341,56 +418,64 @@ var ok_layout = [
     style: {col_bg:RED},
     label: ""
   },
-  // {
-  //   type: "btn",
-  //   origin: {
-  //     x: 3,
-  //     y: 3
-  //   },
-  //   size: {w:40, h:40},
-  //   style: {col_bg:RED},
-  //   label: ""
-  // },
 ]
+
 
 // add it to the layer
 // layer_buttons.add_features(grid_layout);
 layer_buttons.add_features(ok_layout);
+layer_palette.add_features(grid_layout);
 
 // Tap away!
-layer_buttons.layout[0].set_style(
-  {
-    col_bg: WHITE,
-    col_text:BLACK,
-    col_border:PURPLE
-  }
-).set_label("Tap away!").set_origin({x:3, y:3})
+layer_buttons.layout[0].set_style(TXT_STYLE_DEF).set_label("Tap away!").set_origin({x:3, y:3})
 .set_size({w:_screen_mid*2-8,h:_screen_mid-3});
+
 // Palette Tool
 layer_buttons.layout[1].set_style(
   {
     col_bg: WHITE,
-    col_text:BLACK,
-    col_border:PURPLE
+    col_text:BLUE,
+    col_border:GREEN
   }
 ).set_label("Palette Tool").set_origin({x:3, y:_screen_mid+3})
 .set_size({w:_screen_mid*2-8,h:_screen_mid-6});
 
+var splash_cb = function (obj, window) {
+  console.log("Callback");
+
+  let sty = obj.style;
+  console.log(`sty: ${JSON.stringify(sty)}`)
+  
+  g.setColor(sty.col_border);
+  g.fillRect(0, 0, g.getWidth(), g.getHeight());
+  let prev_color = this.style;
+  console.log(`prev_color: ${JSON.stringify(prev_color)}`)
+  
+  obj.set_style({col_bg:obj.style.col_border});
+  obj.render();
+  console.log(`new_color: ${JSON.stringify(this.style)}`);
+  // obj.set_style(prev_color);
+  g.flip(true);
+
+  queueNextTick(function() {
+    console.log("clearing");
+    tick();
+  }, 4000);
+
+};
+
+layer_buttons.layout[0].callback = splash_cb;
+layer_buttons.layout[1].callback = splash_cb;
+
 main_window.push(layer_buttons);
+// main_window.push(layer_palette);
 main_window.render();
 
 console.log(`Created all.`)
 
-Bangle.on('touch', function (button, xy) {
 
-  console.log(`Tapped ${JSON.stringify(xy)}`)
-  // window.render();
-
-});
-
-function draw() {
-  main_window.render();
-}
+Bangle.setLocked(false);
 
 // Tick
-tick();
+// tick();
+draw();
